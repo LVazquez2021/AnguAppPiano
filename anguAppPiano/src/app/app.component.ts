aab48fb10314b0144dac623f889b87280d4f06fd
ElDiez <luisvazquezcontadorpublico@gmail.com>
2022-02-12 21:48:58 -0300

    se agrego la funcionalidad de sonido a cada tecla

--- anguAppPiano/src/app/app.component.ts
+++ anguAppPiano/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'anguapppiano';
+  title = 'piano';
+  aplicarSonido(numero:number):void{
+    const audio = new Audio();
+    audio.src = '../assets/sonidos/note'+ numero +'.wav';
+    audio.load();
+    audio.play();
+  }
 }
